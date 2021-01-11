ff6a25e888ba87911cc103dad172720d739a669f
dat99 <mvdat1999@gmail.com>
2021-01-11 16:40:33 +0700

    bai_tap

---
+++ module2/src/bai1_introduction_to_java/bai_tap/ung_dung_chuyen_doi_tien_te/Money.java
@@ -0,0 +1,14 @@
+package bai1_introduction_to_java.bai_tap.ung_dung_chuyen_doi_tien_te;
+
+import java.util.Scanner;
+
+public class Money {
+    public static void main(String[] args) {
+        System.out.println("Nhập tỉ giá USD muốn chuyển sang VND: ");
+        Scanner scanner =new Scanner(System.in);
+        int USD=scanner.nextInt();
+
+        int rate = 23000*USD;
+        System.out.println("Rate:"+rate);
+    }
+}
