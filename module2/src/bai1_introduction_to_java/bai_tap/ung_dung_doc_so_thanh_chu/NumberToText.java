5dd38afa38bed5256b4c389bba84f331edeac18e
dat99 <mvdat1999@gmail.com>
2021-01-11 16:32:14 +0700

    bai_tap

--- module2/src/bai1_introduction_to_java/bai_tap/ung_dung_doc_so_thanh_chu/NumberToText.java
+++ module2/src/bai1_introduction_to_java/bai_tap/ung_dung_doc_so_thanh_chu/NumberToText.java
@@ -1,7 +1,29 @@
 package bai1_introduction_to_java.bai_tap.ung_dung_doc_so_thanh_chu;
 
+import java.util.Scanner;
+
 public class NumberToText {
     public static void main(String[] args) {
-        
+        // đọc số nguyên dương có tối đa 3 chữ số
+        System.out.println("Số cần đọc là: ");
+        Scanner scanner =new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        //đọc các số có 1 chữ số
+        if(number> 0 && number<10){
+            String numberToText;
+            switch (number){
+                case 1:
+                    numberToText ="one";
+                    break;
+                case 2:
+                    numberToText="two";
+                    break;
+                case 3:
+                    numberToText="three";
+                    break;
+            }
+        }
+
     }
 }
