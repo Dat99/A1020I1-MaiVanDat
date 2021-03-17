e8cd1d5780e9ae4d88df54b484173833f8509efd
dat99 <mvdat1999@gmail.com>
2021-03-17 16:45:45 +0700

    bai_tap

--- module2/src/bai19_string_regex/bai_tap/validate_so_dien_thoai/TelephoneExample.java
+++ module2/src/bai19_string_regex/bai_tap/validate_so_dien_thoai/TelephoneExample.java
@@ -1,10 +1,24 @@
 package bai19_string_regex.bai_tap.validate_so_dien_thoai;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TelephoneExample {
-    private static Pattern pattern;
-    private Matcher matcher;
-    private static final String TELEPHONE_REGEX ="^(\\d){2}-[0-9]$";
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        boolean check=true;
+        String REGEX_SDT="[0-9]{2}-[0][0-9]{9}";
+        while (check){
+            System.out.println("Nhập số điện thoại: ");
+            String input=scanner.nextLine();
+            String kq=input.matches(REGEX_SDT)?"Đúng ":"Sai";
+            System.out.println(kq);
+            System.out.println("Số điện thoại bạn muốn nhập lại");
+            String telephone =scanner.nextLine();
+            if(telephone.equalsIgnoreCase("n"))
+                check=false;
+        }
+
+    }
 }
