bdff78a162a9cd59796a02502058a353a9ea2585
dat99 <mvdat1999@gmail.com>
2021-11-08 18:13:06 +0700

    update

--- module4/07_SpringDataRepository/ThucHanh/QuanLiTinhNew/src/main/java/vn/codegym/repository/ProvinceRepository.java
+++
@@ -1,8 +0,0 @@
-package vn.codegym.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import vn.codegym.model.Province;
-
-public interface ProvinceRepository extends JpaRepository<Province, Integer> {
-}
--- module4/08_Validation/BaiTap/ValidateFormDangKi/src/main/java/vn/codegym/repository/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package vn.codegym.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vn.codegym.model.User;
-
-public interface UserRepo extends JpaRepository<User,Integer> {
-
-}
